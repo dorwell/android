5477e7fb5a4e23f5f19b49c9239d955922d061d9
dorwell <dorwell@gmail.com>
2014-08-25 15:59:58 +0800

    Add the button object

--- GitTest/src/com/example/gittest/MainActivity.java
+++ GitTest/src/com/example/gittest/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.gittest;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	Button button_1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_main);
 	}
 
